a66f2806bff7277ab2653f837e9fa736c08285cf
Gilberto Ocampo <ocampo@gate.net>
2020-02-23 19:07:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count (array)
+  array = array.sort_by {|x| x.length}
+end 
+
+def swap_elements (array)
+ array[2], array[1] = array[1], array[2]
+ array
+end
+
+def reverse_array (array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.insert(3, "$")
+end
+
+# "abcd".insert(3, '$')    #=> "ab$cd"
+
+# def my_each(array)
+#   i = 0 
+#  while i < array.length
+#    yield array[i]
+#    i = i + 1
+#  end
+  
+ # array
+# end
 
